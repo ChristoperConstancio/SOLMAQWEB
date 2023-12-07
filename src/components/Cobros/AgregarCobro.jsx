bf97419196a2b8e47fce71e76940291208fd97c1
Christoper <chris_constancio@hotmail.com>
2023-12-07 12:04:04 -0600

    preFinal version 2.1

--- src/components/Cobros/AgregarCobro.jsx
+++ src/components/Cobros/AgregarCobro.jsx
@@ -67,9 +67,10 @@ function AgregarCobro() {
             document.getElementById('nuVenta').value = idVenta
             document.getElementById('nuCobro').value = idCobro
             const ventaInfo = await getVentasNu(idVenta);
-            console.log(ventaInfo)
+            
+
             // Verificar si ventaInfo tiene el atributo nuCobro
-            if (ventaInfo.total !== ventaInfo.saldo) {
+            if (ventaInfo.total != ventaInfo.saldo) {
                 // El atributo nuCobro está presente en ventaInfo
                 document.getElementById('tipo').value = "Liquidacion"
                 setCobro({...cobro, tipoPago : 'Liquidacion'});
